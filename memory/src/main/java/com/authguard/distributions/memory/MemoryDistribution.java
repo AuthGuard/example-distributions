f730d3f0c84e5042c1bde4e29855f57bd6561c9d
Khaled Y.M <kld_at7@hotmail.com>
2020-06-30 19:52:46 +0100

    Separate the distributions and add methods to run them

---
+++ memory/src/main/java/com/authguard/distributions/memory/MemoryDistribution.java
@@ -0,0 +1,9 @@
+package com.authguard.distributions.memory;
+
+import com.authguard.rest.Application;
+
+public class MemoryDistribution {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
---
+++ mongo/src/main/java/com/authguard/distributions/mongo/MongoDistribution.java
@@ -0,0 +1,9 @@
+package com.authguard.distributions.mongo;
+
+import com.authguard.rest.Application;
+
+public class MongoDistribution {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
