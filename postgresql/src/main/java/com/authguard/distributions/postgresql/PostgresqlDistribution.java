2b3bf9cc288523ee235f7bc7895c6112cac8e8cd
Khaled Y.M <kld_at7@hotmail.com>
2020-10-11 15:13:55 +0100

    Postgres distribution

---
+++ postgresql/src/main/java/com/authguard/distributions/postgresql/PostgresqlDistribution.java
@@ -0,0 +1,9 @@
+package com.authguard.distributions.postgresql;
+
+import com.authguard.rest.Application;
+
+public class PostgresqlDistribution {
+    public static void main(final String[] args) {
+        Application.main(args);
+    }
+}
