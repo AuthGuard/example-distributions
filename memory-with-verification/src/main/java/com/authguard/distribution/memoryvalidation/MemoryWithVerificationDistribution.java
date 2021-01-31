fd19f81690f81c9c62d3e7b1adc31b5056249120
Khaled Y.M <kld_at7@hotmail.com>
2021-01-31 16:57:28 +0000

    New module for email verification

---
+++ memory-with-verification/src/main/java/com/authguard/distribution/memoryvalidation/MemoryWithVerificationDistribution.java
@@ -0,0 +1,9 @@
+package com.authguard.distribution.memoryvalidation;
+
+import com.authguard.rest.Application;
+
+public class MemoryWithVerificationDistribution {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
